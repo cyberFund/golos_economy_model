19eb7c6aafd75ebc37d2da1ea7e411ade0a90545
Mikhail Komarov <nemo1369@gmail.com>
2018-02-28 00:02:39 +0300

    Storages split

--- steem_analysis.py
+++ steem_analysis.py
@@ -17,8 +17,7 @@ from steem.post import Post
 from steembase.exceptions import PostDoesNotExist
 
 STEEM_NODES = [
-    'localhost',
-    'https://steemd.steemit.com',
+#    'localhost',
 ]
 
 FILTERED_OPERATIONS = ["vote","comment","transfer","transfer_to_vesting"]
@@ -30,43 +29,49 @@ def process_operation(db, operation):
     logging.info("Processing operation %s", operation['type'])
 
     if operation['type'] == "vote":
-        if db.get(operation['voter'] + "_votes"):
-            db.set(operation['voter'] + "_votes", db.get(operation['voter'] + "_votes") + 1)
+        if db.get(operation['voter']):
+            db.set(operation['voter'], db.get(operation['voter']) + 1)
         else:
-            db.set(operation['voter'] + "_votes", 1)
+            db.set(operation['voter'], 1)
     elif operation['type'] == "comment":
-        if db.get(operation['author'] + "_comments"):
-            db.set(operation['author'] + "_comments", db.get(operation['author'] + "_comments") + 1)
+        if db.get(operation['author']):
+            db.set(operation['author'], db.get(operation['author']) + 1)
         else:
-            db.set(operation['author'] + "_comments", 1)
+            db.set(operation['author'], 1)
     elif operation['type'] == "transfer":
-        if db.get(operation['to'] + "_transfers"):
-            db.set(operation['to'] + "_transfers", db.get(operation['to'] + "_transfers") + 1)
+        if db.get(operation['to']):
+            db.set(operation['to'], db.get(operation['to']) + 1)
         else:
-            db.set(operation['to'] + "_transfers", 1)
+            db.set(operation['to'], 1)
     elif operation['type'] == "transfer_to_vesting":
-        if db.get(operation['to'] + "_vesting_transfers"):
-            db.set(operation['to'] + "_vesting_transfers", db.get(operation['to'] + "_vesting_transfers") + 1)
+        if db.get(operation['to']):
+            db.set(operation['to'], db.get(operation['to']) + 1)
         else:
-            db.set(operation['to'] + "_vesting_transfers", 1)
+            db.set(operation['to'], 1)
 
-def fill_storage(storage, chain, first_block, last_block):
+def fill(databases, chain, first_block, last_block):
     for operation in chain.history(start_block = first_block, end_block = last_block, filter_by = FILTERED_OPERATIONS):
-        process_operation(storage, operation)
+        process_operation(databases[operation['type']], operation)
 
 def main():
-    db = pickledb.load('steem_analysis.db', False)
+    votes_db = pickledb.load('steem_account_votes.db', False)
+    comments_db = pickledb.load('steem_account_comments.db', False)
+    transfers_db = pickledb.load('steem_account_trasnfers.db', False)
+    vesting_transfers_db = pickledb.load('steem_account_vesting_transfers.db', False)
 
     steemd = Steem(STEEM_NODES)
     chain = Blockchain(steemd_instance = steemd, mode = 'head')
 
     logging.getLogger().setLevel(20)
 
-    fill_storage(db, chain, round((1497970800 - 1451606400) / 3), chain.info()['head_block_number'])
+    databases = {'vote': votes_db, 'comment': comments_db, 'transfer': transfers_db, 'transfer_to_vesting': vesting_transfers_db}
 
-    db.dump()
+    fill(databases, chain, round((1497970800 - 1451606400) / 3), chain.info()['head_block_number'])
 
-    analyze(db)
+    votes_db.dump()
+    comments_db.dump()
+    transfers_db.dump()
+    vesting_transfers_db.dump()
 
 
 if __name__ == "__main__":
